c3c8cc6a8142d3f7724f2ae7daa809539d777570
fahri09 <fahrifahri09@gmail.com>
2020-05-01 19:15:59 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variabel
+
+    String nama = "Fahri";
+    int jumlahSks = 24;
+    double indeksPresentasiKumulatif = 3.89;
+
+}
